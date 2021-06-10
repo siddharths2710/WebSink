95af740cf73f20d26bb1abaf7aeb9c4e9d646c43
Siddharth Srinivasan <siddharths2710@yahoo.com>
2021-06-10 20:55:57 +0530

    Implemented Floating Menu UX

--- app/src/main/java/com/example/websink/MainActivity.kt
+++ app/src/main/java/com/example/websink/MainActivity.kt
@@ -9,13 +9,19 @@ import android.webkit.WebSettings
 import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.chuckerteam.chucker.api.Chucker
 import com.chuckerteam.chucker.api.ChuckerCollector
 import com.chuckerteam.chucker.api.ChuckerInterceptor
 import com.chuckerteam.chucker.api.RetentionManager
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.lang.Exception
+import java.lang.IllegalArgumentException
 
 /*
 * Functionalities of WebSink Web Honeypot:
@@ -24,29 +30,106 @@ import okhttp3.Request
 * 3) Traffic inspection
 * */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var httpClient: OkHttpClient
+    private lateinit var submitBtn: Button
+    private lateinit var browserView: WebView
+    private lateinit var inputAddressField: EditText
+    private lateinit var mMenuFab: ExtendedFloatingActionButton
+    private lateinit var mInspectFab: FloatingActionButton
+    private lateinit var mPinFab: FloatingActionButton
+    private lateinit var mOverrideFab: FloatingActionButton
+    private lateinit var mInspectText: TextView
+    private lateinit var mPinText: TextView
+    private lateinit var mOverrideText: TextView
+    private var areFabsVisibile: Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val httpClient = getHttpClient()
-        val browserView = getBrowserView(R.id.web_sink)
-        val inputAddressField = findViewById<EditText>(R.id.url_bar)
-        val submitBtn = findViewById<Button>(R.id.submit)
-        val domainRegex = Regex("^(((([A-Za-z0-9]+){1,63}\\.)|(([A-Za-z0-9]+(\\-)+[A-Za-z0-9]+){1,63}\\.))+){1,255}$")
+        configureMenuBehaviourInit()
+        httpClient = getHttpClient()
+        browserView = getBrowserView(R.id.web_sink)
+        inputAddressField = findViewById<EditText>(R.id.url_bar)
+        submitBtn = findViewById<Button>(R.id.submit)
         browserView.webViewClient = CustomWebViewClient(applicationContext, httpClient)
         browserView.loadUrl("https://ipchicken.com")
+
         submitBtn.setOnClickListener {
             val addr = inputAddressField.text.toString()
-            val notifyInvalidAddr = Toast.makeText(applicationContext, "Invalid FQDN $addr", Toast.LENGTH_SHORT)
-            if(addr.isNotEmpty() && domainRegex.containsMatchIn(addr)) {
+            val notifyInvalidAddr = Toast.makeText(applicationContext, "Invalid URL $addr", Toast.LENGTH_SHORT)
+            if(addr.isNotEmpty() && isValidFqdn(addr)) {
                 val req = Request.Builder().url(addr).build()
-                val resp = httpClient.newCall(req).execute()
-                browserView.loadDataWithBaseURL(addr, resp.body().toString(), "text/html", "UTF-8", null)
+                try {
+                    val resp = httpClient.newCall(req).execute()
+                    browserView.loadDataWithBaseURL(addr, resp.body().toString(), "text/html", "UTF-8", null)
+                } catch (e: IllegalArgumentException) {
+                    //notifyInvalidAddr.show()
+                    e.printStackTrace()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+
             } else {
                 notifyInvalidAddr.show()
             }
-
         }
+
+    }
+    private fun configureMenuBehaviourInit() {
+        mMenuFab = findViewById(R.id.menu_efab)
+        mInspectFab = findViewById(R.id.inspect_fab)
+        mPinFab = findViewById(R.id.pin_fab)
+        mOverrideFab = findViewById(R.id.override_fab)
+        mInspectText = findViewById(R.id.inspect_text)
+        mOverrideText = findViewById(R.id.override_text)
+        mPinText = findViewById(R.id.pin_text)
+
+        mMenuFab.shrink()
+        toggleVisibility()
+
+        mMenuFab.setOnClickListener {
+            if(!areFabsVisibile) {
+                mMenuFab.extend()
+                toggleVisibility()
+                areFabsVisibile = true
+            } else {
+                mMenuFab.shrink()
+                toggleVisibility()
+                areFabsVisibile = false
+            }
+        }
+
+        mInspectFab.setOnClickListener {
+            //Inspect traffic
+            startActivity(Chucker.getLaunchIntent(applicationContext))
+        }
+
+        mPinFab.setOnClickListener {
+            Toast.makeText(applicationContext, "Cert Pin", Toast.LENGTH_LONG)
+        }
+
+        mOverrideFab.setOnClickListener {
+            Toast.makeText(applicationContext, "Domain Override", Toast.LENGTH_SHORT)
+        }
+
+    }
+
+    private fun toggleVisibility() {
+        val isVisibleObj = if(areFabsVisibile) View.VISIBLE else View.GONE
+        mInspectFab.visibility = isVisibleObj
+        mPinFab.visibility = isVisibleObj
+        mOverrideFab.visibility = isVisibleObj
+        mInspectText.visibility = isVisibleObj
+        mPinText.visibility = isVisibleObj
+        mOverrideText.visibility = isVisibleObj
+    }
+
+    private fun isValidFqdn(domain: String): Boolean {
+        //val domainRegex = Regex("^(((([A-Za-z0-9]+){1,63}\\.)|(([A-Za-z0-9]+(\\-)+[A-Za-z0-9]+){1,63}\\.))+){1,255}$")
+        return true
     }
 
     private fun getHttpClient(): OkHttpClient {
@@ -56,7 +139,7 @@ class MainActivity : AppCompatActivity() {
         return okHttpClient
     }
 
-    private fun getInterceptor: ChuckerInterceptor {
+    private fun getInterceptor(): ChuckerInterceptor {
         return ChuckerInterceptor.Builder(applicationContext)
             .collector(getCollector())
             .maxContentLength(250000L)
